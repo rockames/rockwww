8b3f29a9b7adab477a2eef4ac8d74a80fba781bd
Brian Peck <bpeck@iastate.edu>
2013-09-02 16:37:14 -0500

    Major rewrite to use poet+jade+bootstrap.
    Initial rewrite, closer to an actual website.
    
    Uses poet for blogging.  Posts can simply be put into /_posts.
     - Beginning of file is json metadata
     - actual content of post is in markdown
    
    Actual website is templated in jade
     - Weird language to allow for easy(ish) page generation.
     - One page simply defines layout for whole site (nice!), others do
       individual pages.

---
+++ _posts/test.md
@@ -0,0 +1,20 @@
+{{{
+    "title"    : "Testing a post!",
+    "tags"     : [ "test" ],
+    "category" : "rock",
+    "date"     : "9-02-2013",
+    "preview"  : "This is a test post"
+
+}}}
+
+## This is a test post, does it work?  ##
+
+This is me testing a post.
+
+It has markdown, which is 
+
+ - awesome
+ - really neat.
+ - and pretty cool.
+
+I hope you like the site!
--- web.js
+++ web.js
@@ -1,15 +1,45 @@
-var express = require('express');
-var fs = require('fs');
+var
+  express = require('express'),
+  app = express(),
+  Poet = require('poet');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  var buf = fs.readFileSync('index.html');
-  var data = buf.toString('utf-8');
-  response.send(data);
+var poet = Poet(app, {
+  posts: './_posts/',
+  postsPerPage: 5,
+  metaFormat: 'json'
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+poet.init().then(function () {
+  // initialized
 });
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(app.router);
+
+app.get('/', function (req, res) { 
+  res.render('index');
+});
+
+app.get('/about', function (req, res) {
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+
+app.get('/contact', function (req, res) {
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+
+app.get('/blog', function (req, res) {
+  res.render('blog', {
+    title: 'Blog'
+  });
+});
+
+app.listen(8080);
