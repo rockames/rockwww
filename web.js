1cf5afbec377981c818cadca57f4b6697f3722aa
Brian Peck <bpeck@iastate.edu>
2013-09-02 21:22:17 -0500

    Change listen port

--- web.js
+++ web.js
@@ -42,4 +42,7 @@ app.get('/blog', function (req, res) {
   });
 });
 
-app.listen(8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
